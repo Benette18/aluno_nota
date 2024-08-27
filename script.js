45e2e53827d3428344e26a00b9d15af26062a1be
Benette18 <166033666+Benette18@users.noreply.github.com>
2024-08-27 10:12:44 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+//Passo 1: Criar a constantes que irão armazenar as notas do aluno
+
+const nota_1= 10
+const nota_2= 5
+const nota_4= 4
+
+//Passo 2: Calcular a média a notas
+
+const soma_notas = nota_1 + nota_2 + nota_3
+const media = soma_nota / 3
+
+// Passo 3: Verifiar se a nota é maior que 6
+
+if (media>= 6) {
+    console.log("0 aluno foi provado")
+}
+se media >= 4 && mdia < 6., console.log("está de prova final")
+else{
+    console.log("0 aluno foi reprovado")
+}
+calcula_nota_alunos(10,5,3)
